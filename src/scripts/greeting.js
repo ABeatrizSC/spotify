6b64a907f54682dc7d871ae9189a36e371608d89
Ana Beatriz Santucci Carmoni <anabeatrizscarmoni@gmail.com>
2024-01-27 00:09:24 -0300

    Update greetings.js

--- src/scripts/greeting.js
+++ src/scripts/greeting.js
@@ -3,9 +3,9 @@ greetingElement = document.getElementById('greeting');
 var now = new Date();
 var hour = now.getHours();
 
-if (hour > 5 && hour < 12) {
+if (hour >= 5 && hour < 12) {
     greetingElement.innerText = 'Bom dia';
-} else if (hour < 19) {
+} else if (hour >= 12 && hour <= 18) {
     greetingElement.innerText = 'Boa tarde';
 } else {
     greetingElement.innerText = 'Boa noite';
